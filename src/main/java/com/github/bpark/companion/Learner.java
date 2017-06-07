a4a54e32466c3564f15a3537091bf501d57fe23c
ksr <ksr@financial.com>
2017-06-07 19:08:15 +0200

    added license info and fixed training evaluation

--- src/main/java/com/github/bpark/companion/Learner.java
+++ src/main/java/com/github/bpark/companion/Learner.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2017 Kurt Sparber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.bpark.companion;
 
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.meta.FilteredClassifier;
@@ -22,15 +38,8 @@ public class Learner {
         }
     }
 
-    public FilteredClassifier evaluate(Instances trainData) throws Exception {
+    public void evaluate(Classifier classifier, Instances trainData) throws Exception {
         trainData.setClassIndex(0);
-        StringToWordVector stringToWordVector = new StringToWordVector();
-        stringToWordVector.setAttributeIndices("last");
-
-        FilteredClassifier classifier = new FilteredClassifier();
-        classifier.setFilter(stringToWordVector);
-
-        classifier.setClassifier(new NaiveBayes());
 
         Evaluation eval = new Evaluation(trainData);
 
@@ -40,11 +49,9 @@ public class Learner {
         System.out.println(eval.toSummaryString());
         System.out.println(eval.toClassDetailsString());
 
-        return classifier;
-
     }
 
-    public FilteredClassifier learn(Instances trainData) throws Exception {
+    public Classifier learn(Instances trainData) throws Exception {
         trainData.setClassIndex(0);
         StringToWordVector filter = new StringToWordVector();
         filter.setAttributeIndices("last");
@@ -62,8 +69,8 @@ public class Learner {
     public static void main(String[] args) throws Exception {
         Learner learner = new Learner();
         Instances instances = learner.loadDataset("src/main/resources/classifications/basic-dialogs.arff");
-        learner.evaluate(instances);
-        FilteredClassifier classifier = learner.learn(instances);
+        Classifier classifier = learner.learn(instances);
+        learner.evaluate(classifier, instances);
 
         SerializationHelper.write("src/main/resources/classifications/basic-dialogs.model", classifier);
     }
