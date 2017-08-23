725f0e51b7fe26fb84fad3f76fb40bacbdf7bb9b
bpark <burt.parkers@googlemail.com>
2017-08-23 12:49:01 +0200

    updated copyright

--- src/main/java/com/github/bpark/companion/ClassificationVerticle.java
+++ src/main/java/com/github/bpark/companion/ClassificationVerticle.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Kurt Sparber
+ * Copyright 2017 bpark
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,6 +35,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * @author bpark
+ */
 public class ClassificationVerticle extends AbstractVerticle {
 
     private static final Logger logger = LoggerFactory.getLogger(ClassificationVerticle.class);
--- src/main/java/com/github/bpark/companion/Learner.java
+++ src/main/java/com/github/bpark/companion/Learner.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Kurt Sparber
+ * Copyright 2017 bpark
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,6 +30,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
+/**
+ * @author bpark
+ */
 public class Learner {
 
     public Instances loadDataset(String fileName) throws IOException {
